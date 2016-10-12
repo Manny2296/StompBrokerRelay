57e7f9a845b8999899b7f472ae767242bebd150e
Andres Vega <2099340@linux67.labinfo.is.escuelaing.edu.co>
2016-10-12 16:14:19 -0500

    Parte 1 y 2

---
+++ src/main/java/edu/eci/arsw/msgbroker/WebsocketsMsbrokerExampleApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.msgbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsMsbrokerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsMsbrokerExampleApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,59 @@
+var stompClient = null;
+
+function connect() {
+    var socket = new SockJS('/stompendpoint');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        
+        stompClient.subscribe('/topic/newpoint', function (data) {
+           
+           var cor = JSON.parse(data.body);
+           canvas = document.getElementById('myCanvas');
+           context = canvas.getContext('2d');   
+           context.beginPath();
+           context.arc(cor.x,cor.y,1,0,2*Math.PI);
+           context.stroke();
+           
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function getMousePos(canvas, evt) {
+var rect = canvas.getBoundingClientRect();
+        return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+        };
+}
+var canvas = null;
+var context = null;
+
+
+
+
+$(document).ready(
+        function () {
+            connect();
+            console.info('connecting to websockets');
+        
+            canvas = document.getElementById('myCanvas');
+                  
+                
+            canvas.addEventListener('mousedown', function(evt) {
+                var mousePos = getMousePos(canvas, evt);
+                stompClient.send("/topic/newpoint", {}, JSON.stringify({x:mousePos.x,y:mousePos.y})); 
+                
+            }, false);
+    }
+                
+                
+);
